f633eee7c7a5b7a254be9d39adcd875f8120c47a
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-07-20 22:28:58 +0200

    delete cabled to front and working

--- frontend/src/components/TodoDetails.js
+++ frontend/src/components/TodoDetails.js
@@ -1,9 +1,24 @@
+import { useTodosContext } from "../hooks/useTodosContext";
+
 const TodoDetails = ({ todo }) => {
+  const { dispatch } = useTodosContext();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/todos/" + todo._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_TODO", payload: json });
+    }
+  };
   return (
     <div className="todo-details">
       <h4>{todo.title}</h4>
       <p>Author: {todo.author}</p>
       <p>Todo: {todo.content}</p>
+      <span onClick={handleClick}>Delete</span>
     </div>
   );
 };
